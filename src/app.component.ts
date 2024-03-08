a08cd541398189fee0bfe4f2f77fa3298d80daa1
ernestlv <ernestolv@hotmail.com>
2024-03-08 17:18:46 -0500

    first commit

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {CitiesService} from './cities.service'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  name = 'World XXX';
+export class AppComponent implements OnInit {
+
+  cities:{ name: string; value: string }[] = [];
+
+  form = new FormGroup({
+    origen: new FormControl('', Validators.required),
+    destination: new FormControl('', Validators.required),
+    range: new FormGroup({
+      start: new FormControl<Date | null>(null, Validators.required),
+      end: new FormControl<Date | null>(null, Validators.required)
+    })
+  });
+
+  constructor(
+    private cityService:CitiesService
+  ){}
+
+  ngOnInit() {
+    this.cityService.getCities().subscribe(cities => this.cities = cities);
+  }
+
+  onSubmit() {
+    alert('XXX!!!!')
+  }
 }
